386afc9f0acea7c8776e0b3d681b2f1f7fd2fb08
Jan Bordo <janek.bordo@gmail.com>
2018-08-19 13:11:45 +0200

    added eslint

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 
 function myMap() {
-  console.log('vive la france!');
-  var paris = new google.maps.LatLng(48.8566140, 2.3522219);
-  var warsaw = new google.maps.LatLng(52.2296756, 21.0122287);
-  var zagreb = new google.maps.LatLng(45.8150108, 15.9819189);
-
-  var mapOptions = {
+  const paris = new google.maps.LatLng(48.8566140, 2.3522219);
+  const warsaw = new google.maps.LatLng(52.2296756, 21.0122287);
+  const zagreb = new google.maps.LatLng(45.8150108, 15.9819189);
+  const mapOptions = {
     center: paris,
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.HYBRID
-  }
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  };
+  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  var marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: mapOptions.center,
     map: map,
     title: 'Bienvenue à Paris'
@@ -44,7 +42,7 @@ function myMap() {
     $('.map-viewer').css({
       'opacity': '1',
       'transition-duration': '0.5s'
-    });
+    }); 
     map.setCenter(zagreb);
     marker.setPosition(zagreb);
     marker.setTitle('Dobrodošli u Zagreb');
